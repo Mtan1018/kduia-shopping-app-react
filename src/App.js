c7bc7b33de199df90e75b9c82b7d3b7b7703a9d5
Miaoyun Tan (Mia Tan) <141455850+Mtan1018@users.noreply.github.com>
2024-01-20 15:23:26 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,18 +14,15 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Shopping App</h1>
                 <div className='row mt-3'> 
-	  				/* Add CartValue component here under */ 
 	  				<div className='col-sm'>
 	  					<CartValue />
 	  				</div>
 	  
-  	  				/* Add Location component here under */ 
 	   				<div className='col-sm'>
 	  					<Location />
 	  				</div>
 				</div>
 
-	  				/* Add ExpenseList component here under */ 
 	  				<h3 className='Shopping Cart'></h3>
 	  				<div className='row'>
 	  					<div className='col-sm'>
@@ -33,7 +30,6 @@ const App = () => {
 	  					</div>
 	  				</div>
 
-	  				/* Add ItemSelected component here under */ 
 	  				<h3 className='mt-3'>Add Items</h3>
 	  				<div className='row mt-3'>
 	  					<div className='col-sm'>
